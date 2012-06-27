984fe918685b60d29ed76f052c2c1e759628374a
unnati <unnatipatil1988@gmail.com>
2012-06-27 19:09:24 +0530

    added to relese1 branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authentication::Application.config.secret_token = '026b0c0bfc26632fe7c24173168f9c2b5f9cd493048927f016451d7d9cb2c2a5c41f841ab0eced6ca195142121e8fc3166f2c7d795020eef44f9cb5c71bb5cad'
