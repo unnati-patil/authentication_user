bf97734ac86c14e1ad47bb701201411449645620
unnati <unnatipatil1988@gmail.com>
2012-06-27 20:08:50 +0530

    validations are done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :password_hash, :password_salt
 
+
+
+  #before_save { |user| user.email = email.downcase }
+  before_save :encrypt_password
+
     attr_accessor :password
     before_save :encrypt_password
 
@@ -8,6 +13,11 @@ class User < ActiveRecord::Base
     validates_presence_of :password, :on => :create
     validates_presence_of :email
     validates_uniqueness_of :email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => true
+
+  validates :password, :presence => true, :length => { :minimum => 6 }
 
     def self.authenticate(email, password)
       user = find_by_email(email)
